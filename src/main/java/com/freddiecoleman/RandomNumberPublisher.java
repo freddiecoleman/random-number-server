c58a535395b42fc482d3277161ecdc82f72e4875
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-13 23:43:44 +0100

    creating random number publisher

---
+++ src/main/java/com/freddiecoleman/RandomNumberPublisher.java
@@ -0,0 +1,27 @@
+package com.freddiecoleman;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import com.pushtechnology.diffusion.api.APIException;
+import com.pushtechnology.diffusion.api.publisher.Publisher;
+
+/**
+ * Main RandomNumberPublisher class
+ * @author Freddie Coleman
+ *
+ */
+public class RandomNumberPublisher extends Publisher {
+	
+	@Override
+	protected void initialLoad() throws APIException {
+		addTopic("numbers");
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new RandomGenerator(this), 1L, 10L, TimeUnit.SECONDS);
+	}
+    
+    @Override
+    protected boolean isStoppable() {
+        return true;
+    }
+
+}
